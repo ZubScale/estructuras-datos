1ebab105bf45c06e5e273837be5ac9e069d4adca
PC08-A3\Alumno <na>
2024-08-30 08:15:24 -0600

    Tarea validacion parentesis

--- metodos_organizacion/src/metodos_organizacion/Main.java
+++ metodos_organizacion/src/metodos_organizacion/Main.java
@@ -4,41 +4,15 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Numeros num = new Numeros();
-		Scanner leer = new Scanner(System.in);
+	        Scanner leer = new Scanner(System.in);
+	        System.out.print("Ingrese la expresión matemática: ");
+	        String expresion = leer.nextLine();
 
-		int[] arreglo = num.getNumeros();
-
-		System.out.println("Arreglo original:");
-		num.imprimirNumeros();
-
-		System.out.println("Escoja un método de ordenamiento:");
-		System.out.println("1: Burbuja");
-		System.out.println("2: Inserción");
-		System.out.println("3: Secuencial");
-
-		String opcion = leer.nextLine();
-
-		switch (opcion) {
-		case "1":
-			Burbuja.ordenarBurbuja(arreglo);
-			System.out.println("Arreglo ordenado:");
-			num.imprimirNumeros();
-			break;
-		case "2":
-			Insercion.ordenarInsercion(arreglo);
-			num.imprimirNumeros();
-			break;
-		case "3":
-			Secuencial.ordenarSeleccion(arreglo);
-			System.out.println("Arreglo ordenado por selección:");
-			num.imprimirNumeros();
-			break;
-		default:
-			System.out.println("Opción no válida.");
-			break;
-		}
-
-		leer.close();
-	}
+	        if (Verificar.esValida(expresion)) {
+	            System.out.println("La expresión es válida.");
+	        } else {
+	            System.out.println("La expresión es inválida.");
+	        }
+	        leer.close();
+	    }
 }
